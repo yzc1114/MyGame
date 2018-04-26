085beea733fa82b4921f1c72ac000638ef8b9c89
yzchnb <1021777674@qq.com>
2018-04-26 18:31:35 +0800

    Resources Update

---
+++ Resources/tile maps/rules_sewers.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="rules_sewers" tilewidth="32" tileheight="32" tilecount="2" columns="2">
+ <image source="C:/Program Files/Tiled/examples/sewer_automap/rules_sewers.png" width="72" height="48"/>
+</tileset>
